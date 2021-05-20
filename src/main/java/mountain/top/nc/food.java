cfd8cee08e0023357a8cfc5fe2b28e9f36a192ec
PiSaucer <33438654+PiSaucer@users.noreply.github.com>
2021-05-20 17:24:05 -0500

    more food
    Co-Authored-By: PiSaucer <33438654+PiSaucer@users.noreply.github.com>
    Co-Authored-By: NewCaledonia <72218010+NewCaledonia@users.noreply.github.com>

--- src/main/java/mountain/top/nc/food.java
+++ src/main/java/mountain/top/nc/food.java
@@ -1,5 +1,6 @@
 package mountain.top.nc;
 
+import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
@@ -7,7 +8,12 @@ import net.minecraft.item.FoodComponent;
 public class food {
     
     public static final FoodComponent APPLE_PIE = (new FoodComponent.Builder()).hunger(10).saturationModifier((float) .4).build();
+    public static final FoodComponent CHOCOLATE_MILK = (new FoodComponent.Builder()).hunger(2).saturationModifier((float) .4).build();
+    public static final FoodComponent HOT_CHOCOLATE = (new FoodComponent.Builder()).hunger(8).saturationModifier((float) .4).statusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 2400, 10), 1.0F).build();
     public static final FoodComponent COKE = (new FoodComponent.Builder()).hunger(20).saturationModifier((float) .4).statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 600, 250), 1.0F).build();
     public static final FoodComponent RAW_WAFFLE = (new FoodComponent.Builder()).hunger(1).saturationModifier((float) .4).build();
+    public static final FoodComponent WAFFLE = (new FoodComponent.Builder()).hunger(4).saturationModifier((float) .4).statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 2400, 10), 1.0F).statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 2400, 10), 1.0F).build();
+    public static final FoodComponent CURRY_POWDER= (new FoodComponent.Builder()).hunger(-2).saturationModifier((float) .4).build();
+    public static final FoodComponent CURRY = (new FoodComponent.Builder()).hunger(9).saturationModifier((float) .4).statusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 300, 4), 1.0F).build();
 
 }
--- src/main/java/mountain/top/nc/mod.java
+++ src/main/java/mountain/top/nc/mod.java
@@ -18,8 +18,13 @@ public class mod implements ModInitializer{
 
 	//Food
 	public static final Item APPLE_PIE = new Item(new Item.Settings().group(ItemGroup.FOOD).food(food.APPLE_PIE));
+	public static final Item CHOCOLATE_MILK = new Item(new Item.Settings().group(ItemGroup.FOOD).food(food.CHOCOLATE_MILK));
+	public static final Item HOT_CHOCOLATE = new Item(new Item.Settings().group(ItemGroup.FOOD).food(food.HOT_CHOCOLATE));
 	public static final Item COKE = new Item(new Item.Settings().group(ItemGroup.FOOD).food(food.COKE));
 	public static final Item RAW_WAFFLE = new Item(new Item.Settings().group(ItemGroup.FOOD).food(food.RAW_WAFFLE));
+	public static final Item WAFFLE = new Item(new Item.Settings().group(ItemGroup.FOOD).food(food.WAFFLE));
+	public static final Item CURRY_POWDER = new Item(new Item.Settings().group(ItemGroup.FOOD).food(food.CURRY_POWDER));
+	public static final Item CURRY= new Item(new Item.Settings().group(ItemGroup.FOOD).food(food.CURRY));
 
 	@Override
 	public void onInitialize() {
@@ -31,7 +36,13 @@ public class mod implements ModInitializer{
 
 		//Food
 		Registry.register(Registry.ITEM, new Identifier("nc", "apple_pie"), APPLE_PIE);
+		Registry.register(Registry.ITEM, new Identifier("nc", "chocolate_milk"), CHOCOLATE_MILK);
+		Registry.register(Registry.ITEM, new Identifier("nc", "hot_chocolate"), HOT_CHOCOLATE);
 		Registry.register(Registry.ITEM, new Identifier("nc", "coke"), COKE);
 		Registry.register(Registry.ITEM, new Identifier("nc", "raw_waffle"), RAW_WAFFLE);
+		Registry.register(Registry.ITEM, new Identifier("nc", "waffle"), WAFFLE);
+		Registry.register(Registry.ITEM, new Identifier("nc", "curry_powder"), CURRY_POWDER);
+		Registry.register(Registry.ITEM, new Identifier("nc", "curry"), CURRY);
+		
 	}
 }
